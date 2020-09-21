2a7963bb6f571380a001deffab44ab77655cd6e4
janarthan s <janathanreddy@gmail.com>
2020-09-21 10:48:05 +0530

    Initial Commit

---
+++ MediYoga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MediYoga
+//
+//  Created by Janarthan Subburaj on 21/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
